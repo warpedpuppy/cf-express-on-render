827393a7ade9aeed63ecef02891a7a80d6bb7c9f
ted <ted@warpedpuppy.com>
2023-01-24 07:47:46 -0800

    add new logs

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 let connected = false;
+
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then( () => {
 	connected = true;
+	console.log('db connected')
 })
 .catch( e => {
 	connected = false;
+	console.log('db not connected')
 })
 
 app.get("/", (req, res) => res.type('html').send('<h1>hello world</h1>'));
-
 
 app.post("/", (req, res) => res.send("post hit"));
 
